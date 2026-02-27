cfa0550fb6dfa67bfb8d8b251ac9a6851e015c54
Vishu240606 <vishnusarathi2006@gmail.com>
2026-02-27 15:18:14 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,44 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import Auth from "./pages/Auth";
-import NotFound from "./pages/NotFound";
 import History from "./pages/History";
+import SendMoney from "./pages/SendMoney";
+import Proximity from "./pages/Proximity";
+import Success from "./pages/Success";
+import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-         <Route path="/history" element={<History />} />
-          <Route path="/" element={<Index />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+
+        <BrowserRouter>
+          <Routes>
+            {/* Main Routes */}
+            <Route path="/" element={<Index />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/history" element={<History />} />
+
+            {/* Payment Flow */}
+            <Route path="/send" element={<SendMoney />} />
+            <Route path="/proximity" element={<Proximity />} />
+            <Route path="/success" element={<Success />} />
+
+            {/* Fallback */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
